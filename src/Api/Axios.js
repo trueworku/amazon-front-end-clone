e195e964d39965ff70aa5d1cd1fa498ee7a37d1a
true <trueworku@gmail.com>
2024-02-20 00:24:30 -0500

    edited the url

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://127.0.0.1:5001/clone-df63b/us-central1/api",
+    // local instance of firebase functions url
+    // baseURL: "http://127.0.0.1:5001/clone-df63b/us-central1/api",
+    // render url/deployed version of amazone-server on render.com
+    baseURL: "https://amazoneclonebackend-zewb.onrender.com"
 });
 
 
